7d3431b29b20b5ec79ed9db81c7cd417138e019a
FOWOPE MAHMUD <abulaswad@FOWOPEs-MacBook-Pro.local>
2024-02-07 08:54:57 -0800

    create a product card

--- shared/ProductCard.tsx
+++ shared/ProductCard.tsx
@@ -1,17 +1,45 @@
-import { View, Image, StyleSheet } from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 const ProductCard = () => {
   return (
-    <View style={styles.productCardContainer}>
-        <Image source={require('../assets/donut.png')} />
-    </View>
-  )
-}
+    <SafeAreaView style={styles.productCardContainer}>
+      <Image source={require("../assets/donut.png")} />
+      <View style={styles.textContainer}>
+        <Text>African Doug...</Text>
+        <Text>$30</Text>
+      </View>
+      <TouchableOpacity style={styles.cartButton}>
+        <Text>Add to Cart</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
 
 const styles = StyleSheet.create({
-    productCardContainer: {
-        backgroundColor: "#ffff"
-    }
-  });
+  productCardContainer: {
+    backgroundColor: "#ffff",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    borderRadius: 10,
+    padding:15
+  },
+  textContainer: {
+    flexDirection: "row",
+    gap:10
+  },
+  cartButton:{
+    backgroundColor:"red",
+    padding:10,
+    borderRadius:15
+  }
+});
